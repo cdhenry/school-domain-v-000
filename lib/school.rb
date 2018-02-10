1ed505379ea82feb493404299f42f354408260ab
Chris Henry <cdhenry@gmail.com>
2018-02-10 18:48:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  def initialize(roster)
+    @roster = roster
+  end
+
+  def add_student(student, grade_level)
+    if @roster.has_key?(grade_level)
+      @roster[:grade_level] << student
+    else
+      @roster => {grade_level => [student]}
+    end
+  end
+
+
+end
